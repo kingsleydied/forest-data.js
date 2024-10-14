9157faf116d851547f6adf45dcceee02673b7ee5
priscilla <priscillacalkins@miprepschool.org>
2024-10-14 11:31:40 -0400

    Add files via upload

---
+++ forest data.js
@@ -0,0 +1,57 @@
+let cityName = "Techville";
+let parkCount = 5;
+let parks = [
+    {
+        name: "Central Park",
+        trees: [
+            { species: "Maple", age: 5, health: "Good", height: 20 },
+            { species: "Birch", age: 7, health: "Good", height: 18 }
+        ],
+        facilities: ["playground", "swimming pool"]
+    },
+    {
+        name: "Riverside Park",
+        trees: [
+            { species: "Oak", age: 30, health: "Excellent", height: 25 },
+            { species: "Redwood", age: 50, health: "Good", height: 100 }
+        ],
+        facilities: ["boating", "fishing"]
+    },
+    {
+        name: "Hilltop Park",
+        trees: [
+            { species: "Pine", age: 10, health: "Fair", height: 15 }
+        ],
+        facilities: ["hiking trails"]
+    }];
+
+
+// Task 1: Using array and object manipulation, change the name of "Riverside Park" to "Riverside Greenspace" for the relevant tree in the Parks Array.
+// Your code here
+
+// Task 2: Utilize array and object manipulation to change the name of the tree species "Maple" to "Sugar Maple" for the relevant tree located in "Central Park" within the parks array.
+// Your code here
+
+// Task 3: Employ array and object manipulation to add a new tree with species "Birch", age 7, health "Good", location "Central Park", and height 18 to the parks array.
+// Your code here
+
+// Task 4: Using array and object manipulation, retrieve a list of all tree species located in "Central Park". Store the list in a variable named centralParkTrees.
+// Your code here
+
+// Task 5: Use code to calculate and store the average age of all the trees in the parks array into a variable named averageTreeAge.
+// Your code here
+
+// Task 6: Use code to determine which tree is the tallest among all the trees in the parks array. Store the tallest tree in a variable named tallestTree.
+// Your code here
+
+// Task 7: Using array and object manipulation, remove the facility "playground" from the facilities array in "Central Park" which is located in the parks array.
+// Your code here
+
+// Task 8: Using code, convert the parks array into a JSON Object and store it into a variable called parksJSON.
+// Your code here
+
+// Task 9: Using console.log, display the name and facilities of the first item in the parks array.
+// Your code here
+
+// Task 10: Using console.log, display the species of the third item in the parks array.
+// Your code here
